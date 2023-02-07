1091553523e93adfe4994297776cc76cef7b672b
YvanMd <yvanmonod@gmail.com>
2023-02-07 21:09:23 +0100

    Update randomizer.c

--- hash-table/randomizer.c
+++ hash-table/randomizer.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
+#define CHAR_ALLOWED                                                           \
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWVXYZ1234567890"
 
 int randomizer(int _min, int _max) {
   int rand_num = 0;
   rand_num = (rand() % (_max - _min + 1)) + _min;
   return rand_num;
+}
+
+char *randName() {
+  char *name = malloc(sizeof(*name));
+  const char ALLOWED[] = CHAR_ALLOWED;
+  for (int i = 0; i < 4; i++) {
+    int c = (rand() % (61 - 0 + 1) + 0);
+    name[i] = ALLOWED[c];
+  }
+  name[4] = '\0';
+
+  return name;
 }
