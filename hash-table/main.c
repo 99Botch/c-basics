9ee45557266d2db5fd47376f9ad756ed57d97559
YvanMd <yvanmonod@gmail.com>
2023-02-07 21:10:05 +0100

    Update main.c

--- hash-table/main.c
+++ hash-table/main.c
@@ -1,37 +1,90 @@
 #include "piles.c"
-#include "randomizer.c"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
+int recur(int _index, char *name);
 
 int main(int argc, char *argv[]) {
   system("clear");
 
-  // setup global var----------------------------------------
+  printf("====== MENU ======\n\n");
+  printf("Pick a value between 1 & 5\n\n");
+  printf("1. Add an Item\n");
+  printf("2. Remove an Item\n");
+  printf("3. Get Items\n");
+  printf("4. Get an Item\n");
+  printf("5. Quit program\n\n");
+
+  // setup global var ---------------------------------------
   Pile *pile = initPile();
   srand(time(NULL));
-  int unpilleElem[5] = {0};
 
-  // pile values --------------------------------------------
-  for (int i = 0; i < 10; i++) {
-    pileUp(pile, randomizer(0, 100));
+  // setup menu selection -----------------------------------
+  int selection = 0, isQuitting = 0;
+  char str[2];
+
+  while (!isQuitting && (selection = getchar()) != '\0') {
+
+    fgets(str, sizeof(selection), stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    switch (selection) {
+    case '1':
+      pileUp(pile, randomizer(0, 100));
+      printf("Item added..\n\n");
+      break;
+
+    case '2':
+      printf("Unpilled items: ");
+      for (int i = 0; i < 10; i++) {
+        printf("%d / ", unpille(pile));
+        hash_table[i] = NULL;
+      }
+      printf("\n\n");
+
+      break;
+
+    case '3':
+      getCells(pile);
+      printf("\n");
+      break;
+
+    case '4':
+      printf("Which item are you looking for? ");
+      char *itemName = malloc(sizeof(itemName));
+
+      fgets(itemName, sizeof(itemName), stdin);
+	  itemName[strcspn(itemName, "\n")] = '\0';
+
+      int index = hashing(itemName);
+
+      if (strcmp(itemName, hash_table[index]->name_id) != 0) {
+        index = recur(index, itemName);
+      }
+
+      printf("%s: %d\n\n", hash_table[index]->name_id,
+             hash_table[index]->number);
+
+      free(itemName);
+      break;
+
+    case '5':
+      printf("Quitting..\n");
+      isQuitting = 1;
+      break;
+
+    default:
+      printf("Invalid input\n");
+      break;
+    }
   }
-  getCells(pile);
-
-  // // unpile values ------------------------------------------
-  // for (int i = 0; i < 5; i++) {
-  //   unpilleElem[i] += unpille(pile);
-  // }
-
-  // printf("List of unpilled elems: ");
-  // for (int i = 0; i < 5; i++) {
-  //   printf("%d ; ", unpilleElem[i]);
-  // }
-  // printf("\n\n");
-
-  // // end result ---------------------------------------------
-  // printf("List of remaining elems: ");
-  // getCells(pile);
-
   return 0;
+}
+
+int recur(int _index, char *_name) {
+  if (hash_table[_index]->name_id != _name) {
+    _index++;
+	printf("%s", _name);
+    return recur(_index, _name);
+  }
+  return _index;
 }
